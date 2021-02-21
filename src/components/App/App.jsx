0fb706f0b428dfdf0d7460a4ace6e252104e2fd6
Alvin Wong <wong.alvin13@gmail.com>
2021-02-20 19:21:05 -0600

    original project crash, reboot, add like button and  counter and render images to DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+import GalleryList from '../GalleryList/GalleryList';
+
+class App extends Component {
+  state = {
+    galleryList: []
+  } // end state
+
+  // on load get gallery
+  componentDidMount() {
+    this.getGallery();
+  } // end componentDidMount
+
+  // function to get gallery
+  getGallery = () => {
+    axios.get('/gallery')
+    .then(response => {
+      this.setState({
+        galleryList: response.data
+      })
+    })
+    .catch(err => {
+      console.log('Unable to retrieve gallery', err);
+    })
+  } // end getGallery
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList galleryList={this.state.galleryList}/>
       </div>
-    );
+    ); // end return
+  } // end render
 }
 
 export default App;
