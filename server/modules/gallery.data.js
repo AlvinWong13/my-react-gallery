cebef2e68987ff422a45dcd45e5958516d69193a
Alvin Wong <wong.alvin13@gmail.com>
2021-02-20 19:50:32 -0600

    update get, put, and add post route

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/mack_birthday.jpg', description: 'Mack has a birthday party at up down', likes: 0 },
+    { id: 3, path: 'images/jessica.jpg', description: 'Photo of Jessica and I on my Birthday', likes: 0 }
 ];
 
 module.exports = galleryItems;
